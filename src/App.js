630181b46cc9ebc6db2d4938e6724afee8ebeff2
Nitin Goyal <goyalnitin634@gmail.com>
2020-12-15 14:31:49 +0530

    removed create-react-app clutter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './components/Header.js';
+import Search from './components/Search.js';
+import CharacterGrid from './components/characters/CharacterGrid.js';
+
 
 function App() {
+  
+  const [items, setItems] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+    const [query, setQuery] = useState('');
+  
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+      
+      console.log(result.data)
+      
+      setItems(result.data)
+      setLoaded(true)
+    }
+    
+    fetchItems()  
+  }, [query])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={loaded} items={items} />
     </div>
   );
 }
